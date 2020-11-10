58bfbe2d13cc82de25daa63529ec5b9fac8b6084
Octavio Garcia Aguirre <octaviogarciaaguirre@gmail.com>
2020-11-10 15:12:36 -0300

    Mapeo de objeto que me olvide en el commit

---
+++ app/CategoriaJuego.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class CategoriaJuego extends Model
+{
+  protected $connection = 'mysql';
+  protected $table = 'categoria_juego';
+  protected $primaryKey = 'id_categoria_juego';
+  protected $visible = array('id_categoria_juego','nombre');
+  public $timestamps = false;
+
+  public function juegos(){
+    return $this->hasMany('App\Juego','id_categoria_juego','id_categoria_juego');
+  }
+}
