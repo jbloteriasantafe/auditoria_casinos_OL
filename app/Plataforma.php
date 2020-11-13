01428408c909f1a54815e5ba9ce99d8f06659544
Octavio Garcia Aguirre <octaviogarciaaguirre@gmail.com>
2020-11-13 12:38:58 -0300

    Me olvide este objeto

---
+++ app/Plataforma.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Plataforma extends Model
+{
+  protected $connection = 'mysql';
+  protected $table = 'plataforma';
+  protected $primaryKey = 'id_plataforma';
+  protected $visible = array('id_plataforma','nombre');
+  public $timestamps = false;
+
+  public function casinos(){
+    return $this->belongsToMany('App\Casino','plataforma_tiene_casino','id_plataforma','id_casino');
+  }
+  public function juegos(){
+    return $this->belongsToMany('App\Juego','plataforma_tiene_juego','id_plataforma','id_juego');
+  }
+}
