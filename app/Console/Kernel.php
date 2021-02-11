b2524ce9eb208de7fe0fd11062c17a900588866c
Octavio Garcia Aguirre <octaviogarciaaguirre@gmail.com>
2021-02-11 11:08:08 -0300

    Eliminando entidades inutiles en Online

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -5,12 +5,6 @@ namespace App\Console;
 use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
 use Illuminate\Support\Facades\DB;
-
-use App\Console\Commands\CalcularDiferenciasIDM;
-use App\Console\Commands\SortearMesas;
-use App\Http\Controllers\Mesas\Importaciones\Mesas\ImportadorController;
-use App\Http\Controllers\Mesas\Aperturas\ABMCRelevamientosAperturaController;
-use App\Http\Controllers\Mesas\Apuestas\GenerarPlanillasController;
 use Carbon\Carbon;
 
 class Kernel extends ConsoleKernel
@@ -21,10 +15,6 @@ class Kernel extends ConsoleKernel
      * @var array
      */
     protected $commands = [
-        Commands\asociarJuegoCasino::class,
-        Commands\packJuego::class,
-        Commands\GenerarRelevamientosApuestas::class,
-        SortearMesas::class,
     ];
 
     /**
@@ -35,64 +25,6 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-
-      $schedule->command('RAM:sortear')
-               //->everyMinute() para pruebas
-               ->dailyAt('00:30')
-               ->appendOutputTo('sorteo.log')
-               ->runInBackground();
-
-      // $schedule->command('RAM:sortear')
-      //         ->dailyAt('12:00')
-      //         ->runInBackground();
-      $schedule->command('RAM:sortear')
-               ->dailyAt('16:30')
-               ->appendOutputTo('sorteo.log')
-               ->runInBackground();
-
-     $schedule->command('RelevamientoApuestas:generar')
-             ->dailyAt('00:45')
-             ->appendOutputTo('sorteoApuestas.log')
-             ->runInBackground();
-
-     $schedule->command('RelevamientoApuestas:generar')
-             ->dailyAt('17:10')
-             ->appendOutputTo('sorteoApuestas.log')
-             ->runInBackground();
-
-     $impController= new ImportadorController;
-     $relevamientoController = new ABMCRelevamientosAperturaController;
-     $generarPlanillasController = new GenerarPlanillasController;
-
-     $schedule->call(function () use ($impController,
-                                      $relevamientoController,
-                                      $generarPlanillasController){
-      $comando = DB::table('comando_a_ejecutar')
-          ->where('fecha_a_ejecutar','>',Carbon::now()->format('Y:m:d H:i:s'))
-          ->get();
-      echo($comando);
-      foreach ($comando as $c) {
-        switch ($c->nombre_comando) {
-          case 'IDM:calcularDiff':
-            $impController->calcularDiffIDM();
-            echo('Calculardiff');
-            break;
-          case 'RAM:sortear':
-            $relevamientoController->sortearMesasCommand();
-            echo('Pedido sortear mesas realizado.');
-            break;
-          case 'RelevamientoApuestas:generar':
-            $generarPlanillasController->generarRelevamientosApuestas();
-            echo('Pedido generar apuestas realizado.');
-            break;
-          default:
-            break;
-        }
-      }
-     })
-     ->everyThirtyMinutes()
-     ->appendOutputTo('pedidos.log')
-     ->runInBackground();
     }
 
     /**
