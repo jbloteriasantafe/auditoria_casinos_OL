13fd3b1c79be98e48611078335324a7e980c1b18
Octavio Garcia Aguirre <octaviogarciaaguirre@gmail.com>
2022-11-30 13:31:56 -0300

    Log Actividades: Cambiar inputs por selects

--- app/Http/Controllers/LogController.php
+++ app/Http/Controllers/LogController.php
@@ -19,7 +19,14 @@ class LogController extends Controller
 
   public function buscarTodo(){
     UsuarioController::getInstancia()->agregarSeccionReciente('Log Actividades' , 'logActividades');
-    return view('seccionLogActividades');//->with('logActividades',$logs);
+    //Agregar indices si estas querys se vuelven muy pesadas
+    $usuarios = DB::table('usuario')->select('nombre')->distinct()
+    ->whereNull('deleted_at')->get()->pluck('nombre')->sort();
+    $tablas = DB::table('log')->select('tabla')->distinct()
+    ->get()->pluck('tabla')->sort();
+    $acciones = DB::table('log')->select('accion')->distinct()
+    ->get()->pluck('accion')->sort();
+    return view('seccionLogActividades',compact('usuarios','tablas','acciones'));
   }
 
   public function guardarLog($accion,$tabla,$id_entidad){
