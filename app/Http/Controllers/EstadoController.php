64f289c9f7a30cc7abc339cc797021203ee7007d
Octavio Garcia Aguirre <octaviogarciaaguirre@gmail.com>
2026-04-30 12:23:57 -0300

    Estado Jugadores: Mejorar el informe demografico (considero yo)

--- app/Http/Controllers/EstadoController.php
+++ app/Http/Controllers/EstadoController.php
@@ -363,60 +363,53 @@ class EstadoController extends Controller
   }
   
 public function informeDemografico(Request $request){
-    return 'implementando!';
-    /*
-    $plats = [];
-    if(isset($request->id_plataforma)){//@TODO: validar permisos usuario
-      if($request->id_plataforma == 'TODAS'){
-        $plats = Plataforma::all()->pluck('id_plataforma')->toArray();
-      }
-      else{
-        $plats = [ $request->id_plataforma ];
-      }
+    $user = UsuarioController::getInstancia()->quienSoy()['usuario'];
+    if($user->plataformas()->where('plataforma.id_plataforma',$request->id_plataforma ?? -1)->count() <= 0)
+      return response()->json(["errores" => ["No puede acceder a la plataforma"]],422);
+      
+    $mes = intval($request->mes ?? 0);
+    $anio = intval($request->anio ?? 0);
+    if($mes == 0 || $anio == 0){
+      return response()->json(["errores" => ["Fecha faltante o formato incorrecto"]],422);
     }
-    else{
-      return 'Seleccione una plataforma';
-    }
+    //Lo necesito para el indice de resumen_mensual
+    //la busqueda igual es sobre todas las monedas
+    $id_tms = \App\TipoMoneda::all()->pluck('id_tipo_moneda');
     
-    $d = cal_days_in_month(CAL_GREGORIAN,intval($request->mes),intval($request->anio);
-    $fecha = str_pad($request->anio,4,'0',STR_PAD_LEFT).'-'
-    .str_pad($request->mes,2,'0',STR_PAD_LEFT).'-'
-    .str_pad($d,2,'0',STR_PAD_LEFT);
-    
-    DB::table('jugador as j')
-    ->whereYear(
-    $edad = 'IFNULL(TIMESTAMPDIFF(YEAR,j.fecha_nacimiento,LAST_DAY(p.fecha)),"-")';
-    
-    $ultimo_producido = DB::table('producido_jugadores as p')
-    ->whereYear('p.fecha','<=',$request->anio)
-    ->whereMonth('p.fecha','<=',$request->mes)
-    ->where($reglas)
-    ->orderBy('p.fecha','desc')
+    //Aproximado... redondea para arriba si jugo con 17 años a inicio de mes... habria que verificar
+    //despues los que tienen 18- años buscando dia a dia sobre cada detalle_producido_jugadores del mes
+    $ultima_fecha_producido_mes = DB::table('producido_jugadores as pj')
+    ->select('pj.fecha')
+    ->where('pj.id_plataforma','=',$request->id_plataforma)
+    ->where(DB::raw('YEAR(pj.fecha)'),'=',$anio)
+    ->where(DB::raw('MONTH(pj.fecha)'),'=',$mes)
+    ->orderBy('pj.fecha','desc')
     ->first();
     
-    if($ultimo_producido === null) return 'No hay producido';
-        
-    $ultima_fecha = $ultimo_producido->fecha;
+    $primer_fecha_producido_mes = DB::table('producido_jugadores as pj')
+    ->select('pj.fecha')
+    ->where('pj.id_plataforma','=',$request->id_plataforma)
+    ->where(DB::raw('YEAR(pj.fecha)'),'=',$anio)
+    ->where(DB::raw('MONTH(pj.fecha)'),'=',$mes)
+    ->orderBy('pj.fecha','asc')
+    ->first();
+            
+    $anio_mes = str_pad($anio,4,'0',STR_PAD_LEFT).'-'.str_pad($mes,2,'0',STR_PAD_LEFT);
+    $primer_dia_mes = $anio_mes.'-01';
+    $ultimo_dia_mes = $anio_mes.'-'.str_pad(cal_days_in_month(CAL_GREGORIAN,$mes,$anio),2,'0',STR_PAD_LEFT);   
+   
+    $ultima_fecha_producido_mes = $ultima_fecha_producido_mes === null? 
+      $ultimo_dia_mes 
+    : $ultima_fecha_producido_mes->fecha;
     
-    $menores_de_18 = DB::table('producido_jugadores as p')
-    ->select('dp.jugador')->distinct()
-    ->join('detalle_producido_jugadores as dp','dp.id_producido_jugadores','=','p.id_producido_jugadores')
-    ->join('plataforma as pl','p.id_plataforma','=','p.id_plataforma')
-    ->leftJoin('jugador as j',function($j){
-      return $j->on('j.id_plataforma','=','p.id_plataforma')
-      ->on('j.codigo','=','dp.jugador')
-      ->on('j.fecha_importacion','<=','p.fecha')
-      ->on('j.valido_hasta','>=','p.fecha');
-    })
-    ->where($reglas)
-    ->whereYear('p.fecha','=',$request->anio)
-    ->whereMonth('p.fecha','=',$request->mes)
-    ->where(function($q){
-      return $q->where('dp.apuesta','<>',0)->orWhere('dp.premio','<>',0)->orWhere('dp.beneficio','<>',0);
-    })
-    ->where(DB::raw($edad),'<',18)
-    ->where(DB::raw($edad),'<>','-') 
-    ->get();
+    $primer_fecha_producido_mes = $primer_fecha_producido_mes === null? 
+      $primer_dia_mes 
+    : $primer_fecha_producido_mes->fecha;
+    
+    //Uso la ultima base de datos informada en el mes seleccionado
+    $edad_max = "TIMESTAMPDIFF(YEAR,j.fecha_nacimiento,'$primer_fecha_producido_mes')";
+    $edad_min = "TIMESTAMPDIFF(YEAR,j.fecha_nacimiento,'$ultima_fecha_producido_mes')";
+    $edad_avg = "((COALESCE($edad_max,$edad_min)+COALESCE($edad_min,$edad_max))/2.0)";
     
     $sexo = 'CASE
      WHEN j.sexo IS NULL       THEN "-"
@@ -425,59 +418,127 @@ public function informeDemografico(Request $request){
      ELSE                           "X"
     END';
     
-    $data = DB::table('producido_jugadores as p')
-    ->selectRaw("p.id_plataforma,$sexo as sexo,$edad as edad,COUNT(distinct dp.jugador) as cantidad")
-    ->join('detalle_producido_jugadores as dp','dp.id_producido_jugadores','=','p.id_producido_jugadores')
-    ->leftJoin('jugador as j',function($j){
-      return $j->on('j.id_plataforma','=','p.id_plataforma')->on('j.codigo','=','dp.jugador')->whereNull('j.valido_hasta');
-    })
-    ->where($reglas)
-    ->whereYear('p.fecha','=',$request->anio)
-    ->whereMonth('p.fecha','=',$request->mes)
-    ->where(function($q){
-      return $q->where('dp.apuesta','<>',0)->orWhere('dp.premio','<>',0)->orWhere('dp.beneficio','<>',0);
-    })
-    ->groupBy(DB::raw("p.id_plataforma,$sexo,$edad"))
-    ->orderBy('sexo','asc')
-    ->orderBy('edad','asc')
-    ->get()
-    ->groupBy('sexo')->map(function($d) use (&$menores_de_18) {
-      return $d->groupBy(function($d2){
-        if($d2->edad == '-') return '-';
-        if($d2->edad <  18) return '-18';
-        if($d2->edad <= 35) return '18-35';
-        if($d2->edad <= 45) return '36-45';
-        if($d2->edad <= 55) return '46-55';
-        if($d2->edad <= 65) return '56-65';
-        if($d2->edad <= 75) return '66-75';
-        return '76+';
-      })
-      ->sortBy(function($arr,$k){
-        return $k;
-      })->map(function($por_edad){
-        return $por_edad->reduce(function($carry,$d2){
-          return $carry+$d2->cantidad;
-        },0);
+    $q = DB::table('jugador as j')
+    ->where('j.id_plataforma','=',$request->id_plataforma)
+    ->where('j.fecha_importacion','<=',$ultimo_dia_mes)
+    ->where('j.valido_hasta','>=',$ultimo_dia_mes);
+    
+    $selectRaw = "j.codigo as jugador,COALESCE($edad_avg,'-') as edad,$sexo as sexo";
+    
+    $rjts = [];//Lo hago asi para evitar tener que agrupar por jugador y usar MAX() en todas las columnas
+    foreach($id_tms as $id_tm){
+      $rjt = "rj{$id_tm}";
+      $rjts[] = $rjt;
+      $q = $q->leftJoin("resumen_mensual_producido_jugadores as $rjt",function($j) use ($rjt,$request,$id_tm,$primer_dia_mes){
+        return $j->where("$rjt.id_plataforma",'=',$request->id_plataforma)
+        ->where("$rjt.id_tipo_moneda",'=',$id_tm)
+        ->where("$rjt.aniomes",'=',$primer_dia_mes)
+        ->on("$rjt.jugador",'=','j.codigo');
       });
+    }
+    $apuesta   = implode("+",array_map(function($rjt){ return "IFNULL($rjt.apuesta <> 0,0)";   },$rjts));
+    $premio    = implode("+",array_map(function($rjt){ return "IFNULL($rjt.premio <> 0,0)";    },$rjts));
+    $beneficio = implode("+",array_map(function($rjt){ return "IFNULL($rjt.beneficio <> 0,0)"; },$rjts));
+    
+    $selectRaw .= ",($apuesta+$premio+$beneficio) > 0 as jugo";
+    
+    $en_bd = $q->selectRaw($selectRaw)->get();
+    
+    $aux = $en_bd->keyBy('jugador');
+    assert($aux->count() == $en_bd->count());//Sanity check
+    $en_bd = $aux;
+        
+    $no_en_bd = DB::table('resumen_mensual_producido_jugadores as rj')
+    ->selectRaw("rj.jugador as jugador,SUM(IFNULL(rj.apuesta <> 0,0)+IFNULL(rj.premio <> 0,0)+IFNULL(rj.beneficio <> 0,0)) > 0 as jugo")
+    ->leftJoin('jugador as j',function($j) use ($request,$ultimo_dia_mes){
+      return $j->where('j.id_plataforma','=',$request->id_plataforma)
+      ->where('j.fecha_importacion','<=',$ultimo_dia_mes)
+      ->where('j.valido_hasta','>=',$ultimo_dia_mes)
+      ->on('rj.jugador','=','j.codigo');
     })
-    ->sortBy(function($arr,$k){
-      return $k;
+    ->where('rj.id_plataforma','=',$request->id_plataforma)
+    ->where('rj.aniomes','=',$primer_dia_mes)
+    ->whereIn('rj.id_tipo_moneda',$id_tms)
+    ->whereNull('j.codigo')
+    ->groupBy('rj.jugador')
+    ->get()
+    ->keyBy('jugador');
+        
+    $posibles_menores_en_bd = $en_bd->filter(function($j){
+      return $j->edad < 18;
     });
         
-    $anio = $request->anio;
-    $mes  = $request->mes;
-    $plataforma = $request->id_plataforma == 'TODAS'?
-      'Plataformas'
-    : Plataforma::find($request->id_plataforma)->nombre;
+    //Hago una busca mas fina para los menores de 18
+    $menores = DB::table('jugador as j')
+    ->selectRaw("j.codigo as jugador,
+      CASE
+       WHEN MAX(j.sexo) IS NULL       THEN '-'
+       WHEN MAX(j.sexo) LIKE 'hombre' THEN 'HOMBRE'
+       WHEN MAX(j.sexo) LIKE 'mujer'  THEN 'MUJER'
+       ELSE                           'X'
+      END as sexo,
+      TIMESTAMPDIFF(
+        YEAR,
+        MAX(j.fecha_nacimiento),
+        MIN(IF(
+          IFNULL(dpj.apuesta <> 0,0)+IFNULL(dpj.premio <> 0,0)+IFNULL(dpj.beneficio <> 0,0),
+          pj.fecha,
+          '$ultimo_dia_mes'
+        ))
+      ) as edad,
+      SUM(IFNULL(dpj.apuesta <> 0,0)+IFNULL(dpj.premio <> 0,0)+IFNULL(dpj.beneficio <> 0,0)) > 0 as jugo")
+    ->where('j.id_plataforma','=',$request->id_plataforma)
+    ->where('j.fecha_importacion','<=',$ultimo_dia_mes)
+    ->where('j.valido_hasta','>=',$ultimo_dia_mes)
+    ->whereIn('j.codigo',$posibles_menores_en_bd->pluck('jugador'))
+    ->leftJoin('producido_jugadores as pj',function($j) use ($request,$id_tms,$primer_fecha_producido_mes,$ultima_fecha_producido_mes){
+      return $j->where("pj.id_plataforma",'=',$request->id_plataforma)
+      ->whereIn("pj.id_tipo_moneda",$id_tms)
+      ->where("pj.fecha",">=",$primer_fecha_producido_mes)
+      ->where("pj.fecha","<=",$ultima_fecha_producido_mes);
+    })
+    ->leftJoin('detalle_producido_jugadores as dpj',function($j){
+      return $j->on("dpj.id_producido_jugadores",'=',"pj.id_producido_jugadores")
+      ->on("dpj.jugador",'=','j.codigo');
+    })
+    ->groupBy('j.codigo')
+    ->get()
+    ->keyBy('jugador');
     
-    $view = View::make('planillaInformeDemografico',compact('plataforma','anio','mes','data','menores_de_18'));
+    //Corrijo con exactitud la query original
+    foreach($menores as $jugador => &$j){
+      if($j->edad >= 18.0){
+        $en_bd[$jugador]->edad = $j->edad;//Edad exacta!
+        unset($menores[$jugador]);
+      }
+    }
+    
+    $plataforma = Plataforma::find($request->id_plataforma);
+    $cod_plataforma = $plataforma->codigo;
+    $plataforma = $plataforma->nombre;
+    
+    $pF = (new \DateTimeImmutable($primer_fecha_producido_mes))->format('ymd');
+    $uF = (new \DateTimeImmutable($ultima_fecha_producido_mes))->format('ymd');
+    $bdF = DB::table('importacion_estado_jugador')
+    ->select('fecha_importacion')
+    ->where('id_plataforma','=',$request->id_plataforma)
+    ->where('fecha_importacion','<=',$ultimo_dia_mes)
+    ->orderBy('fecha_importacion','desc')
+    ->first();
+    
+    $bdF = $bdF === null? '(sin)' : (new \DateTimeImmutable($bdF->fecha_importacion))->format('ymd');
+    $view = View::make('planillaInformeDemografico',compact('plataforma','anio','mes','en_bd','no_en_bd','menores'));
     $dompdf = new Dompdf();
+    //$dompdf->set_paper('A4', 'landscape');
     $dompdf->set_paper('A4', 'portrait');
     $dompdf->loadHtml($view->render());
     $dompdf->render();
     $font = $dompdf->getFontMetrics()->get_font("helvetica", "regular");
+    //$dompdf->getCanvas()->page_text(20, 565, "[Producido $pF-$uF | Jugadores $bdF]", $font, 10, array(0,0,0));
+    //$dompdf->getCanvas()->page_text(750, 565, "Página {PAGE_NUM} de {PAGE_COUNT}", $font, 10, array(0,0,0));
+    $dompdf->getCanvas()->page_text(20, 815, "[Producido $pF-$uF | Jugadores $bdF]", $font, 10, array(0,0,0));
     $dompdf->getCanvas()->page_text(515, 815, "Página {PAGE_NUM} de {PAGE_COUNT}", $font, 10, array(0,0,0));
-    return $dompdf->stream('planilla.pdf',['Attachment'=>0]);*/
+    return $dompdf->stream("informeDemografico-$cod_plataforma-{$pF}a{$uF}BD{$bdF}.pdf",['Attachment'=>0]);
   }
 }
 
--- resources/views/planillaInformeDemografico.blade.php
+++ resources/views/planillaInformeDemografico.blade.php
@@ -1,15 +1,112 @@
 <?php
-$total = 0;
-foreach($data as $sexo => $por_edad) foreach($por_edad as $grupo => $cantidad)  $total += $cantidad;
 
-$total_por_sexo = [];
-foreach($data as $sexo => $por_edad) foreach($por_edad as $grupo => $cantidad)  $total_por_sexo[$sexo] = ($total_por_sexo[$sexo] ?? 0) + $cantidad;
+$rangos_edad = [
+  [-1,17.99999,'-18'],
+  [17.9999,35.0,'18-35'],
+  [35.0,45.0,'36-45'],
+  [45.0,55.0,'46-55'],
+  [55.0,65.0,'56-65'],
+  [65.0,75.0,'66-75'],
+  [75.0,INF,'75+']
+];
 
-$total_por_edad = [];
-foreach($data as $sexo => $por_edad) foreach($por_edad as $grupo => $cantidad)  $total_por_edad[$grupo] = ($total_por_edad[$grupo] ?? 0) + $cantidad;
+$en_bd = $en_bd->transform(function(&$j) use ($rangos_edad){
+  $edad = floatval($j->edad);
+  assert($edad >= 18 || $edad === null);
+  if($edad === null){
+    $edad = '-';
+  }
+  else{
+    assert($edad >= 18);//No deberia venir ningún menor
+    $entro_en_alguno = false;
+    foreach($rangos_edad as $rango){
+      if($edad > $rango[0] && $edad <= $rango[1]){
+        $edad = $rango[2];
+        $entro_en_alguno = true;
+        break;
+      }
+    }
+  }
+  $j->edad = $edad;
+  return $j;
+});
 
-$total_por_sexo = collect($total_por_sexo)->sortBy(function($arr,$k){return $k;});
-$total_por_edad = collect($total_por_edad)->sortBy(function($arr,$k){return $k;});
+
+$total_en_bd = 0;
+$total_en_bd_jugo = [];
+$total_en_bd_sexo = [];
+$total_en_bd_edad = [];
+$total_en_bd_jugo_sexo = [];
+$total_en_bd_jugo_edad = [];
+$total_en_bd_jugo_sexo_edad = [];
+$total_en_bd_sexo_edad = [];
+
+foreach($en_bd as $j){
+  $total_en_bd++;
+  $total_en_bd_jugo[$j->jugo] = $total_en_bd_jugo[$j->jugo] ?? 0;
+  $total_en_bd_jugo[$j->jugo]++;
+  
+  $total_en_bd_sexo[$j->sexo] = $total_en_bd_sexo[$j->sexo] ?? 0;
+  $total_en_bd_sexo[$j->sexo]++;
+  $total_en_bd_edad[$j->edad] = $total_en_bd_edad[$j->edad] ?? 0;
+  $total_en_bd_edad[$j->edad]++;
+  
+  $total_en_bd_jugo_sexo[$j->jugo] = $total_en_bd_jugo_sexo[$j->jugo] ?? [];
+  $total_en_bd_jugo_sexo[$j->jugo][$j->sexo] = $total_en_bd_jugo_sexo[$j->jugo][$j->sexo] ?? 0;
+  $total_en_bd_jugo_sexo[$j->jugo][$j->sexo]++;
+  
+  $total_en_bd_jugo_edad[$j->jugo] = $total_en_bd_jugo_edad[$j->jugo] ?? [];
+  $total_en_bd_jugo_edad[$j->jugo][$j->edad] = $total_en_bd_jugo_edad[$j->jugo][$j->edad] ?? 0;
+  $total_en_bd_jugo_edad[$j->jugo][$j->edad]++;
+  
+  $total_en_bd_sexo_edad[$j->sexo] = $total_en_bd_sexo_edad[$j->sexo] ?? [];
+  $total_en_bd_sexo_edad[$j->sexo][$j->edad] = $total_en_bd_sexo_edad[$j->sexo][$j->edad] ?? 0;
+  $total_en_bd_sexo_edad[$j->sexo][$j->edad]++;
+  
+  $total_en_bd_jugo_sexo_edad[$j->jugo] = $total_en_bd_jugo_sexo_edad[$j->jugo] ?? [];
+  $total_en_bd_jugo_sexo_edad[$j->jugo][$j->sexo] = $total_en_bd_jugo_sexo_edad[$j->jugo][$j->sexo] ?? [];
+  $total_en_bd_jugo_sexo_edad[$j->jugo][$j->sexo][$j->edad] = $total_en_bd_jugo_sexo_edad[$j->jugo][$j->sexo][$j->edad] ?? 0;
+  $total_en_bd_jugo_sexo_edad[$j->jugo][$j->sexo][$j->edad]++;
+}
+
+$total_en_bd_jugo_sexo_edad['TOTAL'] = $total_en_bd_sexo_edad;//Los pongo asi para poder iterar
+$total_en_bd_jugo_sexo['TOTAL'] = $total_en_bd_sexo;
+$total_en_bd_jugo_edad['TOTAL'] = $total_en_bd_edad;
+$total_en_bd_jugo['TOTAL'] = $total_en_bd;
+
+$total_no_en_bd = 0;
+$total_no_en_bd_jugo = [];
+foreach($no_en_bd as $j){
+  $total_no_en_bd++;
+  $total_no_en_bd_jugo[$j->jugo] = $total_no_en_bd_jugo[$j->jugo] ?? 0;
+  $total_no_en_bd_jugo[$j->jugo]++;
+}
+
+$total_menores = 0;
+$total_menores_jugo = [];
+foreach($menores as $j){
+  $total_menores++;
+  $total_menores_jugo[$j->jugo] = $total_menores_jugo[$j->jugo] ?? 0;
+  $total_menores_jugo[$j->jugo]++;
+}
+
+$jugo_keys = ['TOTAL',1,0];//Si / No
+$sexo_keys = ['HOMBRE','MUJER','X'];//@TODO: recibir del front
+$edad_keys = array_map(function($e){return $e[2];},$rangos_edad);
+
+//Homogeneizo este porque es el unico que toma los tres tipos
+foreach($jugo_keys as $jugo){
+  $total_en_bd_jugo[$jugo]    = $total_en_bd_jugo[$jugo] ?? 0;
+  $total_en_bd_jugo_edad[$jugo] = $total_en_bd_jugo_edad[$jugo] ?? [];
+  $total_en_bd_jugo_sexo[$jugo] = $total_en_bd_jugo_sexo[$jugo] ?? [];
+  $total_en_bd_jugo_sexo_edad[$jugo] = $total_en_bd_jugo_sexo_edad[$jugo] ?? [];
+  $total_no_en_bd_jugo[$jugo] = $total_no_en_bd_jugo[$jugo] ?? 0;
+  $total_menores_jugo[$jugo]  = $total_menores_jugo[$jugo] ?? 0;
+}
+
+$menores = $menores->groupBy('jugo');//Este si tengo que listarlo todos
+$meses = ['','Enero','Febrero','Marzo','Abril','Mayo','Junio','Julio','Agosto','Septiembre','Octubre','Noviembre','Diciembre'];
+$DECIMALES = 4;
 ?>
 
 
@@ -67,14 +164,39 @@ tr:nth-child(even) {
     <link href="css/estiloPlanillaPortrait.css" rel="stylesheet">
   </head>
   <body>
+    @section('encabezado')
     <div class="encabezadoImg">
       <img src="img/logos/banner_2024_portrait.png" width="900">
-      <h2><span>Juegos Online | Informe demográfico {{$plataforma}} - {{$anio}} - {{$mes}}</span></h2>
+      <h2>&nbsp;</h2>
+      <h2><span>Juegos Online | Informe demográfico {{$plataforma}} - {{$anio}} - {{$meses[$mes]}}</span></h2>
     </div>
     <div class="camposTab titulo" style="right:-15px;">FECHA PLANILLA</div>
-    <div class="camposInfo" style="right:0px;"><span><?php $hoy = date('j-m-y / h:i');print_r($hoy); ?></span></div>
+    <?php $planilla = $planilla ?? date('j-m-y / h:i'); ?>
+    <div class="camposInfo" style="right:0px;"></span>{{ $planilla }}</div>
+    @endsection
+    @yield('encabezado')
 
+    @foreach($jugo_keys as $jugo)
+    <?php 
+      $Tjugo_sexo_edad = $total_en_bd_jugo_sexo_edad[$jugo] ?? [];
+      $Tjugo_sexo = $total_en_bd_jugo_sexo[$jugo] ?? [];
+      $Tjugo_edad = $total_en_bd_jugo_edad[$jugo] ?? [];
+      $Tjugo = $total_en_bd_jugo[$jugo] ?? 0;
+    ?>
+    @if(!$loop->first)
+    <div style="page-break-after: always;"></div>
+    @yield('encabezado')
+    @endif
     <table style="table-layout: fixed;">
+      <tr>
+        @if($jugo === 1)
+        <th class="tablaInicio center" colspan="6">JUGARON</th>
+        @elseif($jugo === 0)
+        <th class="tablaInicio center" colspan="6">NO JUGARON</th>
+        @elseif($jugo == 'TOTAL')
+        <th class="tablaInicio center" colspan="6">TOTAL</th>
+        @endif
+      </tr>
       <tr>
         <th class="tablaInicio center">SEXO</th>
         <th class="tablaInicio center">CANTIDAD</th>
@@ -84,42 +206,157 @@ tr:nth-child(even) {
         <th class="tablaInicio center">%</th>
       </tr>
 
-      @foreach($data as $sexo => $por_edad) 
-      @foreach($por_edad as $grupo => $cantidad)
+      @foreach($sexo_keys as $sexo) 
+      <?php 
+        $Tjugosexo_edad = $Tjugo_sexo_edad[$sexo] ?? [];
+        $Tjugosexo = $Tjugo_sexo[$sexo] ?? 0;
+      ?>
+      @foreach($edad_keys as $edad)
+      <?php 
+        $Tjugosexoedad = $Tjugosexo_edad[$edad] ?? 0;
+      ?>
       <tr>
         @if($loop->first)
-        <th class="tablaInicio center forzar_blanco" rowspan={{count($por_edad)}}>{{$sexo}}</th>
-        <td class="tablaCampos center forzar_blanco" rowspan={{count($por_edad)}}>{{$total_por_sexo[$sexo] ?? 0}}</td>
-        <td class="tablaCampos center forzar_blanco total_horizontal" rowspan={{count($por_edad)}}>{{number_format(100*($total_por_sexo[$sexo] ?? 0)/$total,3,',','.')}}%</td>
+        <?php 
+          $pje = $total_en_bd?
+            number_format(100*$Tjugosexo/$total_en_bd,$DECIMALES,',','.')
+          : 100;
+        ?>
+        <th class="tablaInicio center forzar_blanco" rowspan={{count($edad_keys)}}>{{$sexo}}</th>
+        <td class="tablaCampos center forzar_blanco" rowspan={{count($edad_keys)}}>{{$Tjugosexo}}</td>
+        <td class="tablaCampos center forzar_blanco total_horizontal" rowspan={{count($edad_keys)}}>{{$pje}}%</td>
         @endif
-        <td class="tablaCampos center {{$loop->last && !$loop->parent->last? 'separador_sexo_edades' : ''}}">{{$grupo}}</td>
-        <td class="tablaCampos center {{$loop->last && !$loop->parent->last? 'separador_sexo_edades' : ''}}">{{$cantidad}}</td>
-        <td class="tablaCampos center {{$loop->last && !$loop->parent->last? 'separador_sexo_edades' : ''}}">{{number_format(100*$cantidad/$total,3,',','.')}}%</td>
+        <?php 
+          $pje = $total_en_bd?
+            number_format(100*$Tjugosexoedad/$total_en_bd,$DECIMALES,',','.')
+          : 100;
+        ?>
+        <td class="tablaCampos center {{$loop->last && !$loop->parent->last? 'separador_sexo_edades' : ''}}">{{$edad}}</td>
+        <td class="tablaCampos center {{$loop->last && !$loop->parent->last? 'separador_sexo_edades' : ''}}">{{$Tjugosexoedad}}</td>
+        <td class="tablaCampos center {{$loop->last && !$loop->parent->last? 'separador_sexo_edades' : ''}}">{{$pje}}%</td>
       </tr>
       @endforeach
       @endforeach
-      @foreach($total_por_edad as $grupo => $cantidad)
+      
+      @foreach($edad_keys as $edad)
+      <?php 
+        $Tjugoedad = $Tjugo_edad[$edad] ?? 0;
+      ?>
       <tr class="total">
         @if($loop->first)
-        <th class="tablaCampos center total forzar_blanco" rowspan={{count($total_por_edad)}}>TOTAL</th>
-        <td class="tablaCampos center total forzar_blanco" rowspan={{count($total_por_edad)}}>{{$total}}</td>
-        <td class="tablaCampos center total forzar_blanco total_horizontal" rowspan={{count($total_por_edad)}}>100%</td>
+        <th class="tablaCampos center total forzar_blanco" rowspan={{count($edad_keys)}}>TOTAL</th>
+        <td class="tablaCampos center total forzar_blanco" rowspan={{count($edad_keys)}}>{{$Tjugo}}</td>
+        <?php 
+          $pje = $total_en_bd?
+            number_format(100*$Tjugo/$total_en_bd,$DECIMALES,',','.')
+            : 100;
+        ?>
+        <td class="tablaCampos center total forzar_blanco total_horizontal" rowspan={{count($edad_keys)}}>{{$pje}}%</td>
         @endif
-        <td class="tablaCampos center {{$loop->first? 'total' : ''}}">{{$grupo}}</td>
-        <td class="tablaCampos center {{$loop->first? 'total' : ''}}">{{$cantidad}}</td>
-        <td class="tablaCampos center {{$loop->first? 'total' : ''}}">{{number_format(100*$cantidad/$total,3,',','.')}}%</td>
+        <td class="tablaCampos center {{$loop->first? 'total' : ''}}">{{$edad}}</td>
+        <td class="tablaCampos center {{$loop->first? 'total' : ''}}">{{$Tjugoedad}}</td>
+        <?php 
+        $pje = $total_en_bd?
+          number_format(100*$Tjugoedad/$total_en_bd,$DECIMALES,',','.')
+          : 100;
+        ?>
+        <td class="tablaCampos center {{$loop->first? 'total' : ''}}">{{$pje}}%</td>
       </tr>
       @endforeach
     </table>
     
-    @if(count($menores_de_18 ?? []) > 0)
-      <p>Menores de edad jugando en plataforma:</p>
-      @foreach($menores_de_18 as $m)
-      <p>{{$m->jugador}}</p>
+    @endforeach
+    
+    <?php 
+      $jugo_keys = array_diff($jugo_keys,['TOTAL']);
+    ?>
+    <div style="page-break-after: always;"></div>
+    @yield('encabezado')
+    <table style="table-layout: fixed;">
+      <tr>
+        <th class="tablaInicio center">EN BASE DE DATOS</th>
+        <th class="tablaInicio center">CANTIDAD</th>
+        <th class="tablaInicio center total_horizontal">%</th>
+        <th class="tablaInicio center">JUGARON</th>
+        <th class="tablaInicio center">CANTIDAD</th>
+        <th class="tablaInicio center">%</th>
+      </tr>
+      
+      <?php $total = $total_en_bd+$total_no_en_bd; ?>
+      
+      @foreach($jugo_keys as $jugo)
+      <?php $T = $total_en_bd_jugo[$jugo] ?? 0; ?>
+      <tr>
+        @if($loop->first)
+        <th class="tablaInicio center forzar_blanco" rowspan="{{count($jugo_keys)}}">SÍ</th>
+        <td class="tablaInicio center forzar_blanco" rowspan="{{count($jugo_keys)}}">{{$total_en_bd}}</td>
+        <td class="tablaInicio center forzar_blanco total_horizontal" rowspan="{{count($jugo_keys)}}">{{@number_format(100*($total_en_bd/$total),$DECIMALES,',','.')}}%</td>
+        @endif
+        <td class="tablaInicio center {{$loop->last? 'separador_sexo_edades' : ''}}">{{$jugo? 'SÍ' : 'NO'}}</td>
+        <td class="tablaCampos center {{$loop->last? 'separador_sexo_edades' : ''}}">{{$T}}</td>
+        <td class="tablaCampos center {{$loop->last? 'separador_sexo_edades' : ''}}">{{@number_format(100*$T/$total,$DECIMALES,',','.')}}%</td>
+      </tr>
       @endforeach
-    @else
-    <p>No se encuentran menores de edad jugando en la plataforma</p>
-    @endif
-
+      
+      @foreach($jugo_keys as $jugo)
+      <?php $T = $total_no_en_bd_jugo[$jugo] ?? 0; ?>
+      <tr>
+        @if($loop->first)
+        <th class="tablaInicio center forzar_blanco" rowspan="{{count($jugo_keys)}}">NO</th>
+        <td class="tablaInicio center forzar_blanco" rowspan="{{count($jugo_keys)}}">{{$total_no_en_bd}}</th>
+        <td class="tablaInicio center forzar_blanco total_horizontal" rowspan="{{count($jugo_keys)}}">{{@number_format(100*($total_no_en_bd/$total),$DECIMALES,',','.')}}%</td>
+        @endif
+        <td class="tablaInicio center">{{$jugo? 'SÍ' : 'NO'}}</td>
+        <td class="tablaCampos center">{{$T}}</td>
+        <td class="tablaCampos center">{{@number_format(100*$T/$total,$DECIMALES,',','.')}}%</td>
+      </tr>
+      @endforeach
+      
+      @foreach($jugo_keys as $jugo)
+      <tr class="total">
+        @if($loop->first)
+        <th class="tablaCampos center total forzar_blanco" rowspan="{{count($jugo_keys)}}">TOTAL</th>
+        <td class="tablaCampos center total forzar_blanco" rowspan="{{count($jugo_keys)}}">{{$total}}</td>
+        <td class="tablaCampos center total forzar_blanco total_horizontal" rowspan="{{count($jugo_keys)}}">100%</td>
+        @endif
+        <?php $Tjugo = ($total_en_bd_jugo[$jugo] ?? 0) + ($total_no_en_bd_jugo[$jugo] ?? 0);?>
+        <td class="tablaCampos center {{$loop->first? 'total' : ''}}">{{$jugo? 'SÍ' : 'NO'}}</td>
+        <td class="tablaCampos center {{$loop->first? 'total' : ''}}">{{$Tjugo}}</td>
+        <td class="tablaCampos center {{$loop->first? 'total' : ''}}">{{@number_format(100*$Tjugo/$total,$DECIMALES,',','.')}}%</td>
+      </tr>
+      @endforeach
+    </table>
+    
+    <div style="page-break-after: always;"></div>
+    @yield('encabezado')
+    <table>
+      <tr>
+        <th class="tablaInicio center" colspan="3">Menores de edad</th>
+      </tr>
+    @foreach($jugo_keys as $jugo)
+      <tr>
+        <th class="tablaInicio center" colspan="3">REGISTRADOS {{$jugo? 'JUGANDO' : 'SIN JUGAR'}}</th>
+      </tr>
+      
+      @if($total_menores_jugo[$jugo] ?? 0)
+      <tr>
+        <th class="tablaInicio center">Jugador</th>
+        <th class="tablaInicio center">Sexo</th>
+        <th class="tablaInicio center">Edad</th>
+      </tr>
+        @foreach(($menores[$jugo] ?? []) as $m)
+      <tr>
+        <td class="tablaCampos center">{{$m->jugador}}</td>
+        <td class="tablaCampos center">{{$m->sexo}}</td>
+        <td class="tablaCampos center">{{$m->edad}}</td>
+      </tr>
+        @endforeach
+      @else
+      <tr>
+        <td class="tablaCampos center" colspan="3">No se encuentran jugadores</td>
+      </tr>
+      @endif
+    @endforeach
+    </table>
   </body>
 </html>
